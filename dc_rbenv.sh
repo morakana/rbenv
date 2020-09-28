1328ff47ac719b59084e9fd16dc06f01b19998f3
morakana <morakana@example.com>
2020-09-28 21:22:51 +0900

    dc_rbenv.sh update

--- dc_rbenv.sh
+++ dc_rbenv.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 link_path=`readlink $0`
 if [ -n "$link_path" ]; then
-	script_path=$link_path
+  script_path=$link_path
 else
-	script_path=`realpath $0`
+  script_path=`realpath $0`
 fi
 cd `dirname $script_path`
 docker-compose exec rbenv bash
+
